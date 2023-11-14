15371bea20be60d3eae8736675658c14e9dbcf08
nullmatawasoradesu <119107850+wakaree@users.noreply.github.com>
2023-11-14 19:08:53 +0200

    Update chat_member_2x.py

--- examples/chat_member_2x.py
+++ examples/chat_member_2x.py
@@ -13,12 +13,24 @@ dp = Dispatcher(bot)
     ~F.old_chat_member.is_chat_member(),
     F.new_chat_member.is_chat_member(),
     F.chat.type.in_([ChatType.GROUP, ChatType.SUPERGROUP])
-    # https://docs.aiogram.dev/en/dev-3.x/dispatcher/filters/magic_filters.html
+    # More about magic filters: https://docs.aiogram.dev/en/dev-3.x/dispatcher/filters/magic_filters.html
 )
 async def new_chat_member(update: ChatMemberUpdated) -> Any:
     await bot.send_message(
         chat_id=update.chat.id,
         text=f"Hello, {update.from_user.get_mention()}"
+    )
+
+
+@dp.chat_member_handler(
+    F.old_chat_member.is_chat_member(),
+    ~F.new_chat_member.is_chat_member(),
+    F.chat.type.in_([ChatType.GROUP, ChatType.SUPERGROUP])
+)
+async def left_chat_member(update: ChatMemberUpdated) -> Any:
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=f"Bye, {update.from_user.get_mention()}!"
     )
 
 
